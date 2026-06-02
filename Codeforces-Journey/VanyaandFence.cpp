024bd29876373f29ef7706cc8af05d2e372ffc67
sox-123 <studyyt9279@gmail.com>
2026-06-02 11:25:45 +0530

    VanyaandFence

--- Codeforces-Journey/VanyaandFence.cpp
+++ Codeforces-Journey/VanyaandFence.cpp
@@ -2,6 +2,22 @@
 using namespace std;
 
 int main(){
-    
+    int n,h; cin>>n>>h;
+    vector<int> height;
+    for(int i=0;i<n;i++){
+        int in;
+        cin>>in;
+        height.push_back(in);
+    }
+    int count =0;
+    for(int n : height){
+        if(n>h){
+            count+=2;
+        }
+        else{
+            count++;
+        }
+    }
+    cout<<count;
     return 0;
 }
