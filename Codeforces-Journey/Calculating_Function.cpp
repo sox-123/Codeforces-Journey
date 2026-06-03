d7b8294428c8f99941a91327b6628c63cdfcbd96
sox-123 <studyyt9279@gmail.com>
2026-06-03 15:46:04 +0530

    a

--- Codeforces-Journey/Calculating_Function.cpp
+++ Codeforces-Journey/Calculating_Function.cpp
@@ -1,22 +1,14 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n; cin>>n;
-    int f=0;
-    if(n%2==0){
-        int even,odd;
-        even += ((n*n)/2);
-        odd += (n*n)/4;
-        f = even - odd;
-        cout<<f;
-    }
-    else{
-        int even,odd;
-        even += (((n-1)*(n-1))/2);
-        odd += ((n-1)*(n-1))/4;
-        f = even - odd - n;
-        cout<<f;
-    }
+int main() {
+    long long n;
+    cin >> n;
+
+    if (n % 2 == 0)
+        cout << n / 2;
+    else
+        cout << -(n + 1) / 2;
+
     return 0;
 }
--- Codeforces-Journey/Chat_room.cpp
+++ Codeforces-Journey/Chat_room.cpp
@@ -1,46 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main() {
     string s;
     cin >> s;
-    bool statement = true;
-    for (int i = 0; i < s.size(); i++)
-    {
-        if (s[i] == 'h')
-        {
-            for (int j = i + 1; j < s.size(); j++)
-            {
-                if (s[j] == 'e')
-                {
-                    for (int k = j + 1; k < s.size(); k++)
-                    {
-                        if (s[k] == 'l')
-                        {
-                            for (int l = k + 1; l < s.size(); l++)
-                            {
-                                if (s[l] == 'l')
-                                {
-                                    for (int m = l + 1; m < s.size(); m++)
-                                    {
-                                        if (s[m] == 'o')
-                                        {
-                                            statement == true;
-                                            cout<<"YES";
-                                            break;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+
+    string hello = "hello";
+    int j = 0;
+
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == hello[j]) {
+            j++;
+        }
+        if (j == 5) {
+            cout << "YES";
+            return 0;
         }
     }
-    if(!statement){
-        cout<<"NO";
-    }
+
+    cout << "NO";
     return 0;
 }
